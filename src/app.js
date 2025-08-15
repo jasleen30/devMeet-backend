fe3432a508355f2b0b22bb78cb9dd37da6757bb9
Jasleen Kaur <jas.kaur97@gmail.com>
2025-08-15 13:41:41 +0530

    created a express server

--- src/app.js
+++ src/app.js
@@ -2,10 +2,18 @@ const express = require("express");
 
 const app = express();
 
-app.use("/", (req, res) =>{
-    res.send("Namaste Everyone!!");
+app.use("/", (req, res)=>{
+    res.send("Hiii Jasleen!!!");
 });
 
-app.listen(3000,() =>{
-    console.log("server is connected");
+app.get("/hello",(req,res) =>{
+    res.send("hello hello hello!");
+});
+
+app.use("/test", (req,res) =>{
+    res.send("Hello from the server");
+});
+
+app.listen(3000, () =>{
+    console.log("Server is connected to port 3000......")
 })
