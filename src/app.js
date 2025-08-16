037b36600fd4781e0d1d64992f2594b2ffbd8163
Jasleen Kaur <jas.kaur97@gmail.com>
2025-08-16 23:59:58 +0530

    season2> Episode 5 writing auth middleware

--- src/app.js
+++ src/app.js
@@ -4,32 +4,26 @@ const app = express();
 
 // app.use("/route",rh,[rh2,rh3],rh4,rh5); rh=> route handler
 
-app.get("/user", (req, res, next) =>{
-    console.log("Handling the route user!!");
-    next();
-},
-(req, res, next) => {
-    console.log("Handling the route user 2!!");
 
-    // res.send("2nd Response!!");
-    next();
-},
-(req, res, next) => {
-    console.log("Handling the route user 3!!");
+const { adminAuth, userAuth } = require("./middlewares/auth");
 
-    // res.send("3rd Response!!");
-    next();
-},
-(req, res, next) =>{
-    console.log("Handling the route user 4!!");
-    // res.send("4th Response!!");
-    next();
-},
-(req, res, next) => {
-    console.log("Handling the route user 5!!");
-    res.send("5th Response!!");
-}
-);
+app.use("/admin", adminAuth);
+
+app.post("/user/login", (req, res) =>{
+    res.send("User logged in successfully!");
+});
+
+app.get("/user/data", userAuth, (req, res) =>{
+    res.send("User Data Sent");
+});
+
+app.get("/admin/getAllData", (req,res) =>{
+    res.send("All Data Sent");
+});
+
+app.get("admin/deleteUser", (req, res) => {
+    res.send("Deleted a user");
+});
 
 app.listen(3000, () =>{
     console.log("Server is connected to port 3000......")
