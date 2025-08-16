9747ba1627047eb7c9288b0dd9e037f43f767ee3
Jasleen Kaur <jas.kaur97@gmail.com>
2025-08-17 02:01:19 +0530

    Season 2> Episode 7 Get, update, delete APIs

--- src/app.js
+++ src/app.js
@@ -5,18 +5,65 @@ const User = require("./model/user");
 
 app.post("/signup", async(req, res) =>{
     //Creating a new instance of the User model
-    const user = new User({
-        firstName:"Sachin",
-        lastName: "Tendulkar",
-        emailId: "sachin@kohli.com",
-        password: "sachin@123"
-    });
+    const user = new User(req.body);
 
     try{
         await user.save();
         res.send("User Added successfully!");
     } catch (err) {
         res.status(400).send("Error saving the user");
+    }
+})
+
+app.get("/user", async(req, res) =>{
+    const userEmail = req.body.emailId;
+    try {
+        console.log(userEmail);
+        const user = await User.findOne({emailId: userEmail});
+        if(!user) {
+            res.status(404).send("User Not found");
+        } else {
+            res.send(user);
+        }
+    } catch (error) {
+        res.status(400).send("Something went wrong");
+    }
+})
+
+// Feed API- GET /feed - get all the users from the database
+app.get("/feed", async(req, res) =>{
+    try {
+        const users = await User.find({});
+        res.send(users);
+    } catch (error) {
+        res.status(400).send("Something went wrong");
+    }
+});
+
+// Delete a user from the database
+app.delete("/user", async (req,res) =>{
+    const userId = req.body.userId;
+    try{
+        const user = await User.findByIdAndDelete({_id: userId});
+        res.send("User deleted successfully");
+    } catch(err) {
+        res.status(400).send("Something went wrong");
+    }
+});
+
+// Update data of the user
+
+app.patch("/user", async(req, res) =>{
+    const userId = req.body.userId;
+    const data = req.body;
+    try {
+        const user = await User.findByIdAndUpdate({_id: userId}, data, {
+            returnDocument: "after",
+            runValidators: true,
+        });
+        res.status(200).send("User supdated successfully");
+    } catch (err) {
+        res.status(400).send("Update failed:" +err.message);
     }
 })
 
