01689613e1218628b60188469a1775f3f7924b2c
Jasleen Kaur <jas.kaur97@gmail.com>
2025-08-17 01:25:10 +0530

    Season 2> Episode 6 Init Database

--- src/app.js
+++ src/app.js
@@ -1,27 +1,37 @@
 const express = require("express");
-
+const connectDB = require("./config/database");
 const app = express();
+const User = require("./model/user");
 
-app.use("/", (err, req, res, next) =>{
-    if(err) {
-        res.status(500).send("Something went wrong");
-    }
-});
+app.post("/signup", async(req, res) =>{
+    //Creating a new instance of the User model
+    const user = new User({
+        firstName:"Sachin",
+        lastName: "Tendulkar",
+        emailId: "sachin@kohli.com",
+        password: "sachin@123"
+    });
 
-app.get("/getUserData", (req, res) =>{
-    ///try {
-        throw new Error("dfjfhkj");
-    //} catch (error) {
-       // res.status(500).send("Some Error contact support team");
-    //}
-});
-
-app.use("/", (err, req, res, next) =>{
-    if(err) {
-        // Log your error
-        res.status(500).send("something went wrong");
+    try{
+        await user.save();
+        res.send("User Added successfully!");
+    } catch (err) {
+        res.status(400).send("Error saving the user");
     }
 })
+
+// Connect to DB
+connectDB()
+    .then(() => {
+        console.log("Database connection established....");
+        app.listen(3000, () =>{
+            console.log("Server is successfully listening on port 3000....");
+        });
+    }).catch((err) =>{
+        console.log("Database cannot be connected!!");
+    });
+
+
 
 app.listen(3000, () =>{
     console.log("Server is connected to port 3000......")
