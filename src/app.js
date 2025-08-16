8ac6ff02848b54acef2d9d6d4a418283d5f8c440
Jasleen Kaur <jas.kaur97@gmail.com>
2025-08-16 18:42:41 +0530

    season2> episode 4: playing with routes

--- src/app.js
+++ src/app.js
@@ -5,9 +5,11 @@ const app = express();
 app.use("/", (req, res)=>{
     res.send("Hiii Jasleen!!!");
 });
+
 // This will only handle GET call to /user, if we take type as GET this will work
-app.get("/user",(req,res) =>{
-    res.send("hello hello hello!");
+app.get("/user/:userId/:name/:password",(req,res) =>{
+    console.log(req.params);
+    res.send({firstName:"Jasleen", lastName:"Kaur"});
 });
 // This will handlde POST call to /user, if we take type as POST this will work
 app.post("/user", async(req,res) =>{
