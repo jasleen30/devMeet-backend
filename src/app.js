2b0f618841af8de08339600f8e2d9b8604e924c7
Jasleen Kaur <jas.kaur97@gmail.com>
2025-08-17 00:59:53 +0530

    season 2> episode 5 -wildcard error handling

--- src/app.js
+++ src/app.js
@@ -2,28 +2,26 @@ const express = require("express");
 
 const app = express();
 
-// app.use("/route",rh,[rh2,rh3],rh4,rh5); rh=> route handler
-
-
-const { adminAuth, userAuth } = require("./middlewares/auth");
-
-app.use("/admin", adminAuth);
-
-app.post("/user/login", (req, res) =>{
-    res.send("User logged in successfully!");
+app.use("/", (err, req, res, next) =>{
+    if(err) {
+        res.status(500).send("Something went wrong");
+    }
 });
 
-app.get("/user/data", userAuth, (req, res) =>{
-    res.send("User Data Sent");
+app.get("/getUserData", (req, res) =>{
+    ///try {
+        throw new Error("dfjfhkj");
+    //} catch (error) {
+       // res.status(500).send("Some Error contact support team");
+    //}
 });
 
-app.get("/admin/getAllData", (req,res) =>{
-    res.send("All Data Sent");
-});
-
-app.get("admin/deleteUser", (req, res) => {
-    res.send("Deleted a user");
-});
+app.use("/", (err, req, res, next) =>{
+    if(err) {
+        // Log your error
+        res.status(500).send("something went wrong");
+    }
+})
 
 app.listen(3000, () =>{
     console.log("Server is connected to port 3000......")
